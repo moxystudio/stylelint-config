6e5c6b5a42d7497aa01e8f4f0498c57702516319
André Cruz <andremiguelcruz@msn.com>
2017-02-25 11:44:35 +0000

    Upgrade to new stylelint & suitcss.
    Additionally add tests!

---
+++ test/index.spec.js
@@ -0,0 +1,38 @@
+/* eslint quote-props:0 */
+
+'use strict';
+
+const stylelint = require('stylelint');
+
+function runStylelintOnGoodFiles() {
+    return stylelint.lint({ files: `${__dirname}/fixtures/**/good-*.css`, formatter: 'string' })
+    .then((result) => {
+        const output = result.output.trim();
+
+        output && console.log(output);
+
+        expect(result.errored).toBe(false);
+    });
+}
+
+function runStylelintOnBadFiles() {
+    return stylelint.lint({ files: `${__dirname}/fixtures/**/bad-*.css` })
+    .then((result) => {
+        expect(result.errored).toBe(true);
+
+        const output = JSON.parse(result.output);
+
+        output.forEach((entry) => {
+            const expected = require(entry.source.replace(/\.css$/, '.warns.json'));  // eslint-disable-line global-require
+            const actual = entry.warnings.map((warn) => ({ rule: warn.rule, severity: warn.severity }));
+
+            expect(actual).toEqual(expected);
+        });
+    });
+}
+
+// ----------------------------------------------------------
+
+it('should pass on good files', () => runStylelintOnGoodFiles());
+
+it('should fail on bad files', () => runStylelintOnBadFiles());
