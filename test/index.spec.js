0bf154e606f8564ba55d1581887ae77dd10f21b1
André Cruz <andremiguelcruz@msn.com>
2017-04-07 23:44:16 +0100

    Update test files.

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,16 +10,16 @@ it('should pass on all files', () => {
         const output = JSON.parse(result.output);
 
         output.forEach((entry) => {
-            // Build result
+            // Build actual
             const actual = entry.warnings
                 .sort((warn1, warn2) => warn1.line - warn2.line || warn1.column - warn2.column || warn1.rule.localeCompare(warn2.rule))
                 .map((warn) => ({ rule: warn.rule, severity: warn.severity, line: warn.line, column: warn.column }));
 
             // Uncomment line below to rewrite all expected json results
-            require('fs').writeFileSync(entry.source.replace(/\.css$/, '.result.json'), JSON.stringify(actual, null, 2));
+            // require('fs').writeFileSync(entry.source.replace(/\.css$/, '.json'), JSON.stringify(actual, null, 2));
 
             // Read expected
-            const expected = require(entry.source.replace(/\.css$/, '.result.json'));  // eslint-disable-line global-require
+            const expected = require(entry.source.replace(/\.css$/, '.json'));  // eslint-disable-line global-require
 
             expect(actual).toEqual(expected);
         });
