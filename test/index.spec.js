ee75a9aa816686f4852dfaf2258a1bc0fae8eeff
André Cruz <andremiguelcruz@msn.com>
2017-12-24 16:13:19 +0000

    test: use jest snapshots

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,33 +2,23 @@
 
 'use strict';
 
+const path = require('path');
 const stylelint = require('stylelint');
+const glob = require('glob');
 
-it('should pass on all files', () => (
-    stylelint.lint({ files: `${__dirname}/fixtures/**/*.css` })
-    .then((result) => {
+const files = glob.sync(`${__dirname}/fixtures/**/*.css`);
+
+files.forEach((file) =>
+    it(`should pass on ${path.relative(__dirname, file)}`, async () => {
+        const result = await stylelint.lint({ files: file });
         const output = JSON.parse(result.output);
 
         output.forEach((entry) => {
-            const filePath = entry.source;
-
-            // Build actual
-            const actual = entry.warnings
+            const result = entry.warnings
             .sort((warn1, warn2) => warn1.line - warn2.line || warn1.column - warn2.column || warn1.rule.localeCompare(warn2.rule))
             .map((warn) => ({ rule: warn.rule, severity: warn.severity, line: warn.line, column: warn.column }));
 
-            // Uncomment line below to rewrite all expected json results
-            // require('fs').writeFileSync(filePath.replace(/\.css$/, '.json'), JSON.stringify(actual, null, 4));
-
-            // Read expected
-            const expected = require(filePath.replace(/\.css$/, '.json'));  // eslint-disable-line global-require
-
-            try {
-                expect(actual).toEqual(expected);
-            } catch (err) {
-                console.log(`Tests failed at ${filePath}`);
-                throw err;
-            }
+            expect(result).toMatchSnapshot();
         });
     })
-));
+);
