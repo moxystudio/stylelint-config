f7b0cc0ae4503de3c8cb48ebb7ae7f793d9879b4
João Peixoto <34691925+jdiogopeixoto@users.noreply.github.com>
2019-11-18 22:42:26 +0000

    feat: add stylelint-declaration-strict-value and rule for color
    BREAKING CHANGE: we now enforce the use of css variables on certain properties

---
+++ rules/declaration-strict-value.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// See https://github.com/AndyOGo/stylelint-declaration-strict-value
+
+const properties = [
+    '/color/',
+    'fill',
+    'font-family',
+    'font-weight',
+    'stroke',
+    'z-index',
+];
+
+const options = {
+    'ignoreKeywords': ['currentColor', 'transparent', 'inherit'],
+};
+
+module.exports = {
+    'plugins': [
+        'stylelint-declaration-strict-value',
+    ],
+    'rules': {
+        'scale-unlimited/declaration-strict-value': [properties, options],
+    },
+};
