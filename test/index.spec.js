7ac2b726dae325e0e76089033918aa8212ca0678
André Cruz <andremiguelcruz@msn.com>
2020-01-18 00:09:46 +0000

    feat: add compatibility with stylelint 13
    Also:
    
    - Fix tests in windows
    - Update dependencies
    - Add MOXY jest config

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,17 +8,19 @@ const glob = require('glob');
 
 const files = glob.sync(`${__dirname}/fixtures/**/*.css`);
 
-files.forEach((file) =>
-    it(`should pass on ${path.relative(__dirname, file)}`, async () => {
+files.forEach((file) => {
+    const relativeFile = path.relative(__dirname, file).replace(/\\/g, '/');
+
+    it(`should pass on ${relativeFile}`, async () => {
         const result = await stylelint.lint({ files: file });
         const output = JSON.parse(result.output);
 
         output.forEach((entry) => {
             const result = entry.warnings
-            .sort((warn1, warn2) => warn1.line - warn2.line || warn1.column - warn2.column || warn1.rule.localeCompare(warn2.rule))
-            .map((warn) => ({ rule: warn.rule, severity: warn.severity, line: warn.line, column: warn.column }));
+                .sort((warn1, warn2) => warn1.line - warn2.line || warn1.column - warn2.column || warn1.rule.localeCompare(warn2.rule))
+                .map((warn) => ({ rule: warn.rule, severity: warn.severity, line: warn.line, column: warn.column }));
 
             expect(result).toMatchSnapshot();
         });
-    }),
-);
+    });
+});
